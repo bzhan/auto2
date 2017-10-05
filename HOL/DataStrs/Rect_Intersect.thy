52661d07d06eafccaea274acd3edb3bacaaf2358
Bohua Zhan <bohuazhan@gmail.com>
2017-10-05 11:30:22 +0200

    Cleanup of representation of Operation in Rect_Intersect.

--- HOL/DataStrs/Rect_Intersect.thy
+++ HOL/DataStrs/Rect_Intersect.thy
@@ -30,26 +30,29 @@ definition has_rect_overlap :: "('a::linorder) rectangle list \<Rightarrow> bool
 
 section {* INS / DEL operations *}
 
-(* Encode INS as false and DEL as true *)
-datatype ('a::linorder) operation = Operation (pos: 'a) (ty: bool) (idx: nat) (int: "'a interval")
-setup {* add_rewrite_rule_back @{thm operation.collapse} *}
-setup {* add_rewrite_rule @{thm operation.case} *}
+datatype ('a::linorder) operation =
+  INS (pos: 'a) (idx: nat) (int: "'a interval")
+| DEL (pos: 'a) (idx: nat) (int: "'a interval")
+setup {* fold add_rewrite_rule_back @{thms operation.collapse} *}
 setup {* fold add_rewrite_rule @{thms operation.sel} *}
-setup {* add_rewrite_rule @{thm operation.simps(1)} *}
+setup {* fold add_rewrite_rule @{thms operation.simps(1-2)} *}
+setup {* add_resolve_prfstep @{thm operation.simps(3)} *}
+setup {* add_forward_prfstep_cond @{thm operation.disc(1)} [with_term "INS ?x11.0 ?x12.0 ?x13.0"] *}
+setup {* add_forward_prfstep_cond @{thm operation.disc(2)} [with_term "DEL ?x21.0 ?x22.0 ?x23.0"] *}
 
 instantiation operation :: (linorder) linorder begin
 
 definition less: "(a < b) = (if pos a \<noteq> pos b then pos a < pos b else
-                             if ty a \<noteq> ty b then ty a < ty b else
-                             if idx a \<noteq> idx b then idx a < idx b else int a < int b)"
+                             if is_INS a \<noteq> is_INS b then is_INS a > is_INS b
+                             else if idx a \<noteq> idx b then idx a < idx b else int a < int b)"
 definition less_eq: "(a \<le> b) = (if pos a \<noteq> pos b then pos a < pos b else
-                                 if ty a \<noteq> ty b then ty a < ty b else
-                                 if idx a \<noteq> idx b then idx a < idx b else int a \<le> int b)"
+                              if is_INS a \<noteq> is_INS b then is_INS a > is_INS b
+                              else if idx a \<noteq> idx b then idx a < idx b else int a \<le> int b)"
 
 instance proof
   fix x y z :: "'a operation"
   show a: "(x < y) = (x \<le> y \<and> \<not> y \<le> x)"
-    using less local.less_eq by force
+    by (smt Rect_Intersect.less Rect_Intersect.less_eq leD le_cases3 not_less_iff_gr_or_eq)
   show b: "x \<le> x"
     by (simp add: local.less_eq)
   show c: "x \<le> y \<Longrightarrow> y \<le> z \<Longrightarrow> x \<le> z"
@@ -65,19 +68,23 @@ setup {* fold add_rewrite_rule [@{thm less_eq}, @{thm less}] *}
 lemma operation_leD [forward]:
   "(a::('a::linorder operation)) \<le> b \<Longrightarrow> pos a \<le> pos b" by auto2
 
-lemma operation_lessI [backward2]:
-  "pos a = pos b \<Longrightarrow> ty a < ty b \<Longrightarrow> (a::('a::linorder operation)) < b" by auto2
+lemma operation_lessI [backward]:
+  "p1 \<le> p2 \<Longrightarrow> INS p1 n1 i1 < DEL p2 n2 i2"
+@proof
+  @have "is_INS (INS p1 n1 i1) = True"
+  @have "is_INS (DEL p2 n2 i2) = False"
+@qed
 
 setup {* fold del_prfstep_thm [@{thm less_eq}, @{thm less}] *}
 
 section {* Set of operations corresponding to a list of rectangles *}
 
 fun ins_op :: "'a rectangle list \<Rightarrow> nat \<Rightarrow> ('a::linorder) operation" where
-  "ins_op rects i = Operation (low (yint (rects ! i))) False i (xint (rects ! i))"
+  "ins_op rects i = INS (low (yint (rects ! i))) i (xint (rects ! i))"
 setup {* add_rewrite_rule @{thm ins_op.simps} *}
 
 fun del_op :: "'a rectangle list \<Rightarrow> nat \<Rightarrow> ('a::linorder) operation" where
-  "del_op rects i = Operation (high (yint (rects ! i))) True i (xint (rects ! i))"
+  "del_op rects i = DEL (high (yint (rects ! i))) i (xint (rects ! i))"
 setup {* add_rewrite_rule @{thm del_op.simps} *}
 
 definition ins_ops :: "'a rectangle list \<Rightarrow> ('a::linorder) operation list" where [rewrite]:
@@ -126,10 +133,10 @@ lemma set_all_ops_idx [forward]:
   "oper \<in> set (all_ops rects) \<Longrightarrow> idx oper < length rects" by auto2
 
 lemma set_all_ops_ins [forward]:
-  "oper \<in> set (all_ops rects) \<Longrightarrow> \<not> ty oper \<Longrightarrow> oper = ins_op rects (idx oper)" by auto2
+  "INS p n i \<in> set (all_ops rects) \<Longrightarrow> INS p n i = ins_op rects n" by auto2
 
 lemma set_all_ops_del [forward]:
-  "oper \<in> set (all_ops rects) \<Longrightarrow> ty oper \<Longrightarrow> oper = del_op rects (idx oper)" by auto2
+  "DEL p n i \<in> set (all_ops rects) \<Longrightarrow> DEL p n i = del_op rects n" by auto2
 
 lemma ins_in_set_all_ops:
   "i < length rects \<Longrightarrow> ins_op rects i \<in> set (all_ops rects)" by auto2
@@ -180,7 +187,7 @@ lemma apply_ops_set_mem3 [forward]:
 definition has_overlap_at_k :: "('a::linorder) rectangle list \<Rightarrow> nat \<Rightarrow> bool" where [rewrite]:
   "has_overlap_at_k rects k \<longleftrightarrow> (
     let S = apply_ops_k rects k; ops = all_ops rects in
-      \<not>ty (ops ! k) \<and> has_overlap (xints_of rects S) (int (ops ! k)))"
+      is_INS (ops ! k) \<and> has_overlap (xints_of rects S) (int (ops ! k)))"
 setup {* register_wellform_data ("has_overlap_at_k rects k", ["k < length (all_ops rects)"]) *}
 
 lemma has_overlap_at_k_equiv [forward]:
@@ -216,16 +223,11 @@ lemma has_overlap_at_k_equiv2 [resolve]:
   @case "ins_op rects i < ins_op rects j" @with
     @have "i1 < j1"
     @have "j1 < i2" @with @have "ops ! j1 < ops ! i2" @end
-    @case "ops ! i2 \<in> set (take j1 ops)" @with
-      @obtain k' where "k' < j1" "ops ! k' = ops ! i2" @end
     @have "has_overlap_at_k rects j1"
   @end
   @case "ins_op rects j < ins_op rects i" @with
     @have "j1 < i1"
-    @have "i1 < j2" @with @have "ops ! i1 < ops ! j2" @with
-      @case "pos (ops ! i1) = pos (ops ! j2)" @end @end
-    @case "ops ! j2 \<in> set (take i1 ops)" @with
-      @obtain k' where "k' < i1" "ops ! k' = ops ! j2" @end
+    @have "i1 < j2" @with @have "ops ! i1 < ops ! j2" @end
     @have "has_overlap_at_k rects i1"
   @end
 @qed
